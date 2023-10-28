0a9d0f1cd2cb7ea15bf634353712eaa631258b57
ksmithut <github@ksmithut.com>
2023-10-28 11:03:05 -0600

    update index route

--- ksmithut/app/routes/_index.tsx
+++ ksmithut/app/routes/_index.tsx
@@ -1,9 +1,4 @@
-import type {
-  MetaFunction,
-  LoaderFunctionArgs,
-  ActionFunctionArgs
-} from '@remix-run/node'
-import { useLoaderData, useActionData } from '@remix-run/react'
+import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,18 +7,6 @@ export const meta: MetaFunction = () => {
   ]
 }
 
-export async function action({ params, request }: ActionFunctionArgs) {
-  await request.formData()
-  return {}
-}
-
-export async function loader({ params }: LoaderFunctionArgs) {
-  return {}
-}
-
 export default function Index() {
-  const data = useLoaderData<typeof loader>()
-  const actionData = useActionData<typeof action>()
-  console.log(data, actionData)
   return <h1 className="text-3xl font-bold">Hello World</h1>
 }
