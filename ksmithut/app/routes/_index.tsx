5cb17560b6e1501b442c71ef5eb04442e9d5fd2f
ksmithut <github@ksmithut.com>
2023-10-28 11:06:10 -0600

    simplify component

--- ksmithut/app/routes/_index.tsx
+++ ksmithut/app/routes/_index.tsx
@@ -1,12 +1,3 @@
-import type { MetaFunction } from '@remix-run/node'
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'App Title' },
-    { name: 'description', content: 'App Description' }
-  ]
-}
-
 export default function Index() {
   return <h1 className="text-3xl font-bold">Hello World</h1>
 }
